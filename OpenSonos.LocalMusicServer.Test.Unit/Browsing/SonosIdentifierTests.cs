742c4107eae647a46d3eccda20e20b8df4fda4ab
David Whitney <david@davidwhitney.co.uk>
2014-10-09 18:58:19 +0100

    IsDirectory detection tests.

---
+++ OpenSonos.LocalMusicServer.Test.Unit/Browsing/SonosIdentifierTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using OpenSonos.LocalMusicServer.Browsing;
+
+namespace OpenSonos.LocalMusicServer.Test.Unit.Browsing
+{
+    [TestFixture]
+    public class SonosIdentifierTests
+    {
+        [TestCase("\\\\some\\path", true)]
+        [TestCase("\\\\some\\path\\file.mp3", false)]
+        public void CreatedWithAPath_IsDirectoryIsAccurate(string path, bool isDirectory)
+        {
+            var si = new SonosIdentifier {Path = path};
+
+            Assert.That(si.IsDirectory, Is.EqualTo(isDirectory));
+        }
+    }
+}
